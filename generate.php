b98c427c239cb79e1a5d35d769855c8385cb3eb7
MarkMiura <miura@sigme.net>
2022-08-29 15:49:33 +0900

    Initial settings

---
+++ generate.php
@@ -0,0 +1,27 @@
+<?php
+include_once('libraries/parsedown/parsedown.php');
+include_once('manualgenerator.class.php');
+
+$argv = $GLOBALS['argv'];
+
+$template = isset($argv[1]) ? $argv[1] : 'default';
+
+echo 'Including Template: ' . $template . PHP_EOL;
+
+if (file_exists('template/custom/' . $template . '/template.php'))
+    include_once 'template/custom/' . $template . '/template.php';
+else
+    include_once 'template/default/template.php';
+
+// Generates a complete manual.
+$manual = new ManualGenerator(
+    PRODUCT_NAME,
+    PRODUCT_HOME,
+    PRODUCT_SUPPORT_URL,
+    PRODUCT_FAQ_URL,
+    $template);
+
+$manual->build('', 'output/');
+
+// Tar up the images in `output/en/img`
+shell_exec('cd output; tar -czvf images.tar.gz en/img');
